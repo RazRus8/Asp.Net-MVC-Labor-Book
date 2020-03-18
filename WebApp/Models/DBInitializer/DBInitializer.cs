7395d787c323857cc4955f980cdc412d5f4fa3eb
Ruslan Razuvailo <razrus@hotmail.com>
2020-03-18 12:02:09 +0300

    DB initializer added

---
+++ WebApp/Models/DBInitializer/DBInitializer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace WebApp.Models.DBInitializer
+{
+    public class DBInitializer : DropCreateDatabaseAlways<EmployeeContext>
+    {
+        protected override void Seed(EmployeeContext db)
+        {
+            db.SigninUsers.Add(new SigninUserModel { Username = "admin", Password = "1234" });
+
+            base.Seed(db);
+        }
+    }
+}
